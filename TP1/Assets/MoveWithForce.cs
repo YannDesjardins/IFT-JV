6479764a836bdd38b6a85c219523c8379a97db6e
Yann_IFT <yann.benard-desjardins.1@ulaval.ca>
2018-09-29 16:58:11 -0400

    ajout MRUA/gravitation et mouvement avec force

---
+++ TP1/Assets/MoveWithForce.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWithForce : MonoBehaviour {
+
+	private Physics physics;
+	public Vector3 forceToApply;
+	public float durationOfForce;
+
+	// Use this for initialization
+	void Start () {
+		physics = GetComponent<Physics> ();
+		ApplyForce ();
+	}
+	
+	void ApplyForce(){
+		physics.AddForce (forceToApply);
+		Invoke ("StopForce", durationOfForce);
+	}
+
+	void StopForce(){
+		physics.RemoveForce (forceToApply);
+	}
+}
--- TP1/Assets/Physics.cs
+++ TP1/Assets/Physics.cs
@@ -3,21 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Physics : MonoBehaviour {
-	
+
 	public Vector3 velocity;
 	public Vector3 forceTotal;
 	public List<Vector3> forcesToSum = new List<Vector3>();
-	private float mass = 0.5f;
+	private float mass = 5f;
+	private Vector3 acceleration;
+	private Vector3 gravity = new Vector3(0.0f, -9.81f, 0.0f);
 
 	// Use this for initialization
 	void Start () {
-		Vector3 force1 = new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f));
-		Vector3 force2 = new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f));
-		Vector3 force3 = new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f));
 
-		forcesToSum.Add(force1);
-		forcesToSum.Add(force2);
-		forcesToSum.Add(force3);
 	}
 	
 	// Update is called once per frame
@@ -26,9 +22,11 @@ public class Physics : MonoBehaviour {
 	}
 
 	void FixedUpdate () {
+
+		//Algo Cinematique
 		SumForces ();
 		ModifyVelocity ();
-		transform.position += velocity * Time.deltaTime;
+		transform.position += velocity * Time.deltaTime + ( (0.5f * acceleration) * (Mathf.Pow(Time.deltaTime, 2.0f)));
 	}
 
 	void SumForces () {
@@ -40,8 +38,16 @@ public class Physics : MonoBehaviour {
 	}
 
 	void ModifyVelocity () {
-		Vector3 acceleration = forceTotal / mass;
-		velocity += acceleration * Time.deltaTime;
+		acceleration = forceTotal / mass;
+		velocity += (acceleration + gravity) * Time.deltaTime;
+	}
+		
+	public void AddForce (Vector3 force) {
+		forcesToSum.Add (force);
+	}
+
+	public void RemoveForce (Vector3 force) {
+		forcesToSum.Remove (force);
 	}
 }
 
