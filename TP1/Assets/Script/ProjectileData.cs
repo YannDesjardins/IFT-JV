638eaefcbd69be5879a153e3dee90141cd75ed8b
Philippe Hebert <philippe.hebert.4@ulaval.ca>
2018-10-20 13:44:05 -0400

    missing items from last commit

--- TP1/Assets/Script/ProjectileData.cs
+++ TP1/Assets/Script/ProjectileData.cs
@@ -6,16 +6,24 @@ public class ProjectileData : MonoBehaviour {
     public delegate void OnProjectileDestruction();
     public event OnProjectileDestruction onProjectileDestruction;
     public float timeTolive = 4;
+    private AABB aabb;
 
     float time;
 	// Use this for initialization
 	void Start () {
         time = Time.time;
-	}
+
+        GameObject bounds = GameObject.FindWithTag("Bounds");
+
+        if (bounds != null)
+        {
+            aabb = bounds.GetComponent<AABB>();
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Time.time-time > 3)
+        if (Time.time-time > 3 || aabb.IsOutOfBound(gameObject.transform.position))
         {
             Destroy(gameObject);
             if (onProjectileDestruction != null)
