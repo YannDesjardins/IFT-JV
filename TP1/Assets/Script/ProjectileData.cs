cac7dc1d6cdb0e953b6a3ef4ccb5c182adbc74b5
gobergio <beden_men@hotmail.com>
2018-10-20 23:27:37 -0400

    Removed timer on projectiles

--- TP1/Assets/Script/ProjectileData.cs
+++ TP1/Assets/Script/ProjectileData.cs
@@ -5,14 +5,10 @@ using UnityEngine;
 public class ProjectileData : MonoBehaviour {
     public delegate void OnProjectileDestruction();
     public event OnProjectileDestruction onProjectileDestruction;
-    public float timeTolive = 4;
     private AABB aabb;
 
-    float time;
 	// Use this for initialization
 	void Start () {
-        time = Time.time;
-
         GameObject bounds = GameObject.FindWithTag("Bounds");
 
         if (bounds != null)
@@ -23,7 +19,7 @@ public class ProjectileData : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Time.time-time > 3 || aabb.IsOutOfBound(gameObject.transform.position))
+        if (aabb.IsOutOfBound(gameObject.transform.position))
         {
             Destroy(gameObject);
             if (onProjectileDestruction != null)
