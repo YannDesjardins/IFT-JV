e8e495e2e1080917d5751593709b1071c285ab92
Yann_IFT <yann.benard-desjardins.1@ulaval.ca>
2018-11-24 02:04:14 -0500

    loading screen

--- TP2/Assets/Scripts/MainMenu.cs
+++ TP2/Assets/Scripts/MainMenu.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour {
-	
+
+	public GameObject loadingScreen;
+	public Slider loadingSlider;
 
 	void Start (){
 	Time.timeScale = 1;
@@ -14,7 +17,21 @@ public class MainMenu : MonoBehaviour {
 	public void StartGame (){
 
 		StaticGameStats.EnemyCount = StaticGameStats.EnemyMax;
-		SceneManager.LoadScene("Game");
+		StartCoroutine (LoadGameAsync ());
+
+	}
+		
+
+	IEnumerator LoadGameAsync(){
+		AsyncOperation loading = SceneManager.LoadSceneAsync("Game");
+		loadingScreen.SetActive (true);
+
+		while (!loading.isDone) {
+
+			float loadingProgress = Mathf.Clamp01 (loading.progress / .9f);
+			loadingSlider.value = loadingProgress;
+			yield return null;
+		}
 	}
 
 	public void QuitGame (){
