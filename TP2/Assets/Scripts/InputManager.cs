612b251e0960ea2732100f063d160ea8d6875921
Philippe Hebert <philippe.hebert.4@ulaval.ca>
2018-11-24 18:18:42 -0500

    axis emulation & custom controls

---
+++ TP2/Assets/Scripts/InputManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputManager : MonoBehaviour {
+
+    private static Dictionary<string, KeyCode> controls = new Dictionary<string, KeyCode>();
+
+
+	// Use this for initialization
+	void Start () {
+        controls.Add("ForwardButton", KeyCode.W);
+        controls.Add("BackwardButton", KeyCode.S);
+        controls.Add("LeftButton", KeyCode.A);
+        controls.Add("RightButton", KeyCode.D);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
